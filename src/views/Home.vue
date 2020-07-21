b7351b9ca5db501fa6c2b7c1f24225ae129d0afa
Kim LaRocca <kim@kimlarocca.com>
2020-07-21 15:45:49 -0400

    update home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,6 @@
 <template>
   <main class="home l-container">
     <h1>Home page</h1>
-    <basic-card
-      has-cta
-      title="Weekly Music Roundup: Hachalu Hundessa RIP, Monte, and Jorja Smith"
-      title-link="http://www.bing.com"
-      image="./assets/Placeholder-Image-1_1.png"
-      alt-text="alternative text"
-      show="Gig Alerts"
-      show-link="http://www.google.com"
-      date="1/11/2020"
-      duration="30 min"
-      description="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab beatae consectetur corporis dicta magnam, molestias natus nobis, placeat, quae quia suscipit ullam vitae voluptatem? Enim facilis ipsum odio praesentium quis."
-    />
   </main>
 </template>
 
