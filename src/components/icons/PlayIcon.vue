1dbb0fef8c7d201c18289d0b67297d5a1af4ccdb
Kim LaRocca <kim@kimlarocca.com>
2020-09-25 15:35:46 -0400

    lint errors

--- src/components/icons/PlayIcon.vue
+++ src/components/icons/PlayIcon.vue
@@ -1,8 +1,8 @@
 <template>
   <svg
+    id="play-shapes"
     width="100px"
     role="img"
-    id="play-shapes"
     height="100px"
     viewBox="0 0 100 100"
     version="1.1"
@@ -30,14 +30,14 @@
           cy="50"
           r="50"
           fill="none"
-        ></circle>
+        />
         <circle
           class="inner-circle-fx"
           cx="50"
           cy="50"
           r="50"
           fill="none"
-        ></circle>
+        />
       </g>
       <path
         class="outer-circle"
@@ -45,7 +45,7 @@
         fill="none"
         stroke="#FFF"
         fill-rule="nonzero"
-      ></path>
+      />
 
       <path
         d="M50,93 C73.748244,93 93,73.748244 93,50 C93,26.2517558 73.748244,7 50,7 C26.2517558,7 7,26.2517558 7,50 C7,73.748244 26.2517558,93 50,93 L50,93 L50,93 Z"
@@ -53,20 +53,20 @@
         stroke="#000"
         fill="none"
         fill-rule="nonzero"
-      ></path>
+      />
 
       <path
         class="pause"
         d="M54,35 L61,35 L61,67 L54,67 L54,35 Z M38,35 L45,35 L45,67 L38,67 L38,35 Z"
         fill="#FFF"
         fill-rule="nonzero"
-      ></path>
+      />
       <polyline
         class="play"
         stroke="#000"
         fill-rule="nonzero"
         points="40 68 40 34 68 51 40 68"
-      ></polyline>
+      />
     </g>
   </svg>
 </template>
