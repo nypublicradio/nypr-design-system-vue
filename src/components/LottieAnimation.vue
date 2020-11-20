eab68eb85d53112961fb91496c80011f014903e2
Hannah Warren <hannahkwarren@gmail.com>
2020-11-20 11:57:15 -0500

    ‘timestamp-linting’

--- src/components/LottieAnimation.vue
+++ src/components/LottieAnimation.vue
@@ -1,16 +1,20 @@
 <template>
-    <Lottie :options="defaultOptions" :height="26" :width="26"  />
+  <Lottie
+    :options="defaultOptions"
+    :height="26"
+    :width="26"
+  />
 </template>
- 
+
 <script>
 import * as animationData from './animations/AnimationWaveActive.json'
 import Lottie from 'vue-lottie/src/lottie.vue'
- 
+
 export default {
     components: {
         Lottie
     },
-    data() {
+    data () {
         return {
             defaultOptions: {
                 animationData: animationData.default
--- src/components/TimeStamp.vue
+++ src/components/TimeStamp.vue
@@ -1,31 +1,33 @@
 <template>
   <div class="time-stamp">
-    {{ date1 }} <template v-if="date2"><span class="time-stamp-seperator">{{ dateSeperator }}</span>{{ date2Label }} {{ date2 }}</template>
+    {{ date1 }} <template v-if="date2">
+      <span class="time-stamp-seperator">{{ dateSeperator }}</span>{{ date2Label }} {{ date2 }}
+    </template>
   </div>
 </template>
 
 <script>
 export default {
-  name: "TimeStamp",
+  name: 'TimeStamp',
   props: {
     date1: {
       type: String,
-      default: null,
+      default: null
     },
     date2: {
       type: String,
-      default: null,
+      default: null
     },
     date2Label: {
       type: String,
-      default: null,
+      default: null
     },
     dateSeperator: {
       type: String,
-      default: "•",
-    },
-  },
-};
+      default: '•'
+    }
+  }
+}
 </script>
 
 <style lang="scss">
