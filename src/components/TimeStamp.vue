c0d67d14f20ca2ade6bca9722a2ad817d90dccd8
Hannah <hannahkwarren@gmail.com>
2020-11-23 16:05:35 -0500

    includes optional date1Label

--- src/components/TimeStamp.vue
+++ src/components/TimeStamp.vue
@@ -1,33 +1,38 @@
 <template>
   <div class="time-stamp">
-    {{ date1 }} <template v-if="date2">
-      <span class="time-stamp-seperator">{{ dateSeperator }}</span>{{ date2Label }} {{ date2 }}
+    {{ date1Label }} {{ date1 }}
+    <template v-if="date2">
+      <span class="time-stamp-seperator">{{ seperator }}</span>{{ date2Label }} {{ date2 }}
     </template>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'TimeStamp',
+  name: "TimeStamp",
   props: {
+    date1Label: {
+      type: String,
+      default: null,
+    },
     date1: {
       type: String,
-      default: null
+      default: null,
     },
     date2: {
       type: String,
-      default: null
+      default: null,
     },
     date2Label: {
       type: String,
-      default: null
+      default: null,
     },
-    dateSeperator: {
+    seperator: {
       type: String,
-      default: '•'
-    }
-  }
-}
+      default: "•",
+    },
+  },
+};
 </script>
 
 <style lang="scss">
@@ -37,7 +42,7 @@ export default {
   color: RGB(var(--color-text));
 }
 
-.time-stamp-seperator{
-    margin:0 var(--space-2);
+.time-stamp-seperator {
+  margin: 0 var(--space-2);
 }
 </style>
