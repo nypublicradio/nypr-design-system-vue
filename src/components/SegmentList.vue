df1de64c18fc74fe92526d0d495b03aa097d9e65
Kim LaRocca <kim@kimlarocca.com>
2020-12-04 09:25:47 -0500

    product feedback

--- src/components/SegmentList.vue
+++ src/components/SegmentList.vue
@@ -1,29 +1,15 @@
 <template>
   <div class="segment-list">
-    <v-accordion
-      should-open-on-load
-      closed-on-mobile
-    >
-      <template v-slot:header>
-        <h3 class="segment-list-title">
-          Segments
-        </h3>
-      </template>
-      <template v-slot:content>
-        <slot />
-      </template>
-    </v-accordion>
+    <h3 class="segment-list-title">
+      Segments
+    </h3>
+    <slot />
   </div>
 </template>
 
 <script>
-import VAccordion from '../components/VAccordion'
-
 export default {
-  name: 'SegmentList',
-  components: {
-    VAccordion
-  }
+  name: 'SegmentList'
 }
 </script>
 
@@ -32,14 +18,5 @@ export default {
   @include typeface(body, 4);
   text-transform: uppercase;
   font-weight: bold;
-}
-
-.segment-list .accordion-header {
-  justify-content: normal !important;
-  margin-bottom: var(--space-1);
-}
-
-.segment-list .accordion-header-wrapper {
-  width: auto !important;
 }
 </style>
