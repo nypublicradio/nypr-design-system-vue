71eb80b5c15785adf939787bac27ab07ca42059b
Kim LaRocca <kim@kimlarocca.com>
2020-12-18 18:02:04 -0500

    header and footer styles

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -185,9 +185,6 @@ $z-index-header: 5000;
   margin: var(--space-2) 0 var(--space-2) var(--space-2);
   width: 40%;
   height: 40%;
-  @include media("<large") {
-    display: none;
-  }
 }
 
 .c-main-header .menu-hamburger {
