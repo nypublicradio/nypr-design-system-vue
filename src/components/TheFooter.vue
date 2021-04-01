a94d63d9f1411e6a8c2851b5137f95f10fc2a4b8
Kim LaRocca <kim@kimlarocca.com>
2021-04-01 11:33:00 -0400

    design feedback

--- src/components/TheFooter.vue
+++ src/components/TheFooter.vue
@@ -162,7 +162,7 @@ export default {
   lang="scss"
 >
 .c-main-footer {
-  padding: var(--space-5);
+  padding: var(--space-5) var(--space-2);
   background-color: RGB(var(--color-background));
   color: RGB(var(--color-text));
 
