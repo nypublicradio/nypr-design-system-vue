5d059490c0c9cda2da80411bb9558fe9eee23051
Kim LaRocca <kim@kimlarocca.com>
2021-04-02 17:38:42 -0400

    vue error

--- src/components/GalleryPreview.vue
+++ src/components/GalleryPreview.vue
@@ -24,7 +24,7 @@
         :class="{ active: activeIndex === index }"
         @click="setActiveImage(index)"
         @keypress.enter.space.prevent="setActiveImage(index)"
-      />
+      >
       <a
         :href="galleryUrl"
         class="gallery-preview-view-all"
@@ -55,7 +55,7 @@ export default {
     },
     images: {
       type: Array,
-      default: null
+      default: () => []
     },
     variation: {
       type: String,
