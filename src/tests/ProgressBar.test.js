37506c4515671bf69af550ada4c3ad5a122f2626
Kim LaRocca <kim@kimlarocca.com>
2021-04-16 11:30:29 -0400

    Progress Bar Component

---
+++ src/tests/ProgressBar.test.js
@@ -0,0 +1,27 @@
+import { mount } from '@vue/test-utils'
+import { describe, test, expect } from '@jest/globals'
+import ProgressBar from '../components/ProgressBar'
+import { toHaveNoViolations } from 'jest-axe'
+import axe from './axe-helper'
+
+expect.extend(toHaveNoViolations)
+
+describe('ProgressBar', () => {
+
+  test('percent prop works: double', () => {
+    const wrapper = mount(ProgressBar, {
+      propsData: {
+        percent: 50
+      }
+    })
+    // check if size prop works and was rendered correctly
+    const div = wrapper.find('.progress-bar-progress')
+    expect(div.attributes().style).toBe('width: 50%;')
+  })
+
+  test('it passes basic accessibility tests', async () => {
+    const wrapper = mount(ProgressBar)
+    const results = await axe(wrapper.element)
+    expect(results).toHaveNoViolations()
+  })
+})
