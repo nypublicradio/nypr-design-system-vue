2f512fdbb7ef3a9f023d4aa48d3fc33455ae0fec
Thomas Bono <thomasbono.com@gmail.com>
2021-10-25 19:30:45 -0400

    video support

--- src/components/VPerson.vue
+++ src/components/VPerson.vue
@@ -4,10 +4,28 @@
     :class="[hasDetails ? 'has-details' : '', image ? '' : 'no-image', orientation === 'vertical' ? 'vertical' : '', orientation === 'responsive' ? 'responsive' : '']"
     :style="cssVars"
   >
-    <template v-if="nameLink && image">
+    <div
+      v-if="video && showVideo"
+      class="video-holder"
+    >
+      <LazyYoutube
+        ref="youtubeLazyVideo"
+        :autoplay="true"
+        :custom-title="name +`'s introduction video` "
+        thumbnail-quality="medium"
+        class="video"
+        src="https://www.youtube.com/watch?v=LOS5WB75gkY"
+      />
+      <v-button
+        class="mod-small closer"
+        label="X"
+        @click="handleVideoClick($event)"
+      />
+    </div>
+
+    <template v-if=" nameLink && image">
       <nuxt-link
         class="person-link"
-        :class="[circle ? 'circle' : '']"
         :to="nameLink"
         aria-hidden="true"
         role="presentation"
@@ -16,17 +34,33 @@
       >
         <span
           v-if="image"
+          :class="[circle ? 'circle' : '']"
           v-html="theVisualAsset"
         />
+        <v-button
+          v-if="video"
+          class="mod-flat video-play-button"
+          @click="handleVideoClick($event)"
+        >
+          <play-icon />
+        </v-button>
       </nuxt-link>
     </template>
     <template v-else>
       <span
         v-if="image"
+        :class="[circle ? 'circle' : '']"
         class="person-image-holder"
         style="justify-self: center;"
         v-html="theVisualAsset"
       />
+      <v-button
+        v-if="video"
+        class="mod-flat video-play-button"
+        @click="handleVideoClick($event)"
+      >
+        <play-icon />
+      </v-button>
     </template>
     <div
       v-if="hasDetails"
@@ -54,13 +88,26 @@
         class="role"
         v-html="role"
       />
-      <span
-        v-if="blurb"
-        ref="blurbRef"
-        class="blurb collapsed"
+      <div
+        ref="blurbHolderRef"
+        class="blurbHolder"
+      >
+        <div
+          v-if="blurb"
+          ref="blurbRef"
+          class="blurb"
+          v-html="blurb"
+        />
+      </div>
+      <a
+        v-if="truncate"
+        class="read-more"
+        ref="readMoreRef"
         @click="handleBlurb()"
-        v-html="truncBlurb"
-      />
+      >
+        {{ readMore ? 'read less' : 'read more' }}
+      </a>
+
       <share-tools class="social">
         <share-tools-item
           service="facebook"
@@ -80,20 +127,28 @@
         />
       </share-tools>
     </div>
+
   </div>
 </template>
 
 <script>
+import gsap from 'gsap'
+import { LazyYoutube } from 'vue-lazytube'
+import VButton from './VButton'
+import PlayIcon from './icons/PlayIcon'
 import ShareTools from './ShareTools'
 import ShareToolsItem from './ShareToolsItem'
-
+let debounce
 /**
  * A component for displaying details about a person
  */
 export default {
   components: {
     ShareTools,
-    ShareToolsItem
+    ShareToolsItem,
+    VButton,
+    PlayIcon,
+    LazyYoutube
   },
   props: {
     /**
@@ -165,17 +220,26 @@ export default {
     imgScale: {
       type: String,
       default: '100'
+    },
+    /**
+     *  % youtube link to promo video
+     */
+    video: {
+      type: String,
+      default: null
     }
   },
   data () {
     return {
-      readMore: false
+      readMore: false,
+      showVideo: false
     }
   },
   computed: {
     cssVars () {
       return {
-        '--img-scale': this.imgScale + '%'
+        '--img-scale': this.imgScale + '%',
+        '--trunc-lines': this.truncate
       }
     },
     hasDetails () {
@@ -192,30 +256,45 @@ export default {
           decoding="async"
         />
     `
-    },
-    truncBlurb () {
-      if (this.truncate) {
-        const truncValue = this.truncate === true ? 90 : Number(this.truncate)
-
-        if (this.readMore) {
-          return this.blurb + '<a class="read-more">READ LESS</a>'
-        } else {
-          return (
-            this.blurb.substring(0, truncValue) +
-            '...' +
-            '<a class="read-more">READ MORE</a>'
-          )
-        }
-      } else {
-        return this.blurb
-      }
+    }
+  },
+  mounted () {
+    if (this.truncate) {
+      window.addEventListener('resize', this.onResize)
+    }
+  },
+  beforeDestroy () {
+    if (this.truncate) {
+      window.removeEventListener('resize', this.onResize)
     }
   },
   methods: {
     handleBlurb () {
-      const { blurbRef } = this.$refs
+      const { blurbHolderRef, blurbRef } = this.$refs
       this.readMore = !this.readMore
-      blurbRef.classList.toggle('collapsed')
+      blurbRef.classList.toggle('expanded')
+      this.onResize()
+
+      gsap.to(blurbHolderRef, {
+        duration: this.readMore ? 0.5 : 0.15,
+        height: blurbRef.offsetHeight + 5
+      })
+    },
+    onResize () {
+      if (!this.readMore) {
+        clearTimeout(debounce)
+        debounce = setTimeout(() => {
+          const { blurbHolderRef, blurbRef, readMoreRef } = this.$refs
+          const clamped = blurbRef.scrollHeight > blurbRef.clientHeight
+          gsap.set(blurbHolderRef, { height: blurbRef.offsetHeight + 5 })
+          readMoreRef.classList.toggle('show-me', clamped)
+        }, 100)
+      }
+    },
+    handleVideoClick: function (event) {
+      event.preventDefault()
+      event.stopPropagation()
+      this.showVideo = !this.showVideo
     }
   }
 }
@@ -261,7 +340,9 @@ export default {
     align-self: start;
     justify-self: center;
     width: 100%;
-    &.circle {
+    span.circle {
+      position: relative;
+      display: block;
       border-radius: 100%;
       overflow: hidden;
     }
@@ -269,6 +350,25 @@ export default {
   .person-image {
     width: 100%;
     height: auto;
+  }
+  .video-play-button {
+    position: absolute;
+    margin-top: -6%;
+    margin-left: 1%;
+    &:hover {
+      opacity: 1;
+    }
+    svg {
+      &:hover {
+        opacity: 1;
+        .play,
+        .inner-circle {
+          stroke: RGB(var(--color-primary-1));
+        }
+      }
+      width: 40px;
+      height: 40px;
+    }
   }
   .person-details {
     padding: 0 var(--space-3);
@@ -294,21 +394,23 @@ export default {
       word-break: break-word;
       letter-spacing: 1.6px;
     }
-
-    .blurb {
+    .blurbHolder {
       overflow: hidden;
-      transition: max-height 0.3s ease-out; // note that we're transitioning max-height, not height!
-      height: auto;
-      max-height: 600px; // still have to hard-code a value!
-      &.collapsed {
-        //max-height: 200px;
+      .blurb {
+        display: -webkit-box;
+        -webkit-line-clamp: var(--trunc-lines);
+        -webkit-box-orient: vertical;
+        overflow: hidden;
+        margin-top: var(--space-1);
+        font-weight: var(--font-weight-body);
+        font-size: var(--font-size-4);
+        line-height: var(--line-height-1);
+        overflow-wrap: anywhere;
+        word-break: break-word;
+        &.expanded {
+          -webkit-line-clamp: unset;
+        }
       }
-      margin-top: var(--space-1);
-      font-weight: var(--font-weight-body);
-      font-size: var(--font-size-4);
-      line-height: var(--line-height-1);
-      overflow-wrap: anywhere;
-      word-break: break-word;
     }
 
     .social {
@@ -322,6 +424,41 @@ export default {
       color: RGB(var(--color-text));
       font-size: var(--font-size-2);
       letter-spacing: 0.6px;
+      text-transform: uppercase;
+      margin-top: -2px;
+      display: none;
+      &.show-me {
+        display: block;
+      }
+    }
+  }
+  .video-holder {
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    margin: auto;
+    width: 100vw;
+    height: 100vh;
+    background: rgba(0, 0, 0, 0.7);
+    z-index: 15;
+    max-width: 100%;
+    .video {
+      width: 90vw;
+      height: 90vh;
+      max-width: 100%;
+      position: absolute;
+      top: 0;
+      bottom: 0;
+      left: 0;
+      right: 0;
+      margin: auto;
+    }
+    .closer {
+      position: absolute;
+      top: 20px;
+      right: 20px;
     }
   }
 }
