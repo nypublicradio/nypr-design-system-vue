ea1f0f1c73c52c2f1b1bcd5f9f22c66cee4c353c
walsh9 <walsh9@gmail.com>
2022-02-09 16:45:40 -0500

    fix flexiblelink to type, linter cleanup

--- src/tests/FlexibleLink.test.js
+++ src/tests/FlexibleLink.test.js
@@ -3,17 +3,17 @@ import { describe, expect } from '@jest/globals'
 import FlexibleLink from '../components/FlexibleLink'
 
 describe('FlexibleLink', () => {
-  let wrapper = {};
-  
-  // find NuxtLink 
+  let wrapper = {}
+
+  // find NuxtLink
   const findNuxtLink = () => wrapper.find('nuxt-link-stub')
   // find an 'a' tag
   const findAnchor = () => wrapper.find('a')
 
-  //component factory
-  const createComponent = ({ propsData = {} } = {}) => {
+  // component factory
+  const createComponent = ({ props = {} } = {}) => {
     wrapper = shallowMount(FlexibleLink, {
-      propsData,
+      props,
       global: {
         stubs: {
           'nuxt-link': true
@@ -23,31 +23,29 @@ describe('FlexibleLink', () => {
   }
 
   afterEach(() => {
-    if (wrapper.destroy) {
-      wrapper.destroy()
-    } else {
-      wrapper = null;
-    }
+    wrapper.unmount()
   })
 
   it('should render with a relative link', () => {
-    createComponent({ propsData: {
-      to: '/abc' 
-    }})
+    createComponent({
+      props: {
+        to: '/abc'
+      }
+    })
     expect(findNuxtLink().exists()).toBe(true)
     expect(findNuxtLink().attributes('to')).toBe('/abc')
   })
 
-
   it('should render with an external link', () => {
-    createComponent({ propsData:{
-      to: 'https://example.com' 
-    }})
+    createComponent({
+      props: {
+        to: 'https://example.com'
+      }
+    })
     expect(findAnchor().exists()).toBe(true)
     expect(findAnchor().attributes('href')).toBe('https://example.com')
     expect(findAnchor().attributes('target')).toBe('_blank')
     expect(findAnchor().attributes('rel')).toBe('noopener noreferrer')
     expect(findAnchor().attributes('class')).toBe('external')
   })
-
 })
